840a626b400ade0dcc31f267ee25dec24270fff8
Anthony Whyte <whyte.jnr@gmail.com>
2019-10-14 19:36:42 -0300

    latest update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const Kraken = require('./kraken')
 
+
 const client = new Kraken()
 
 // client.public('ticker', {pair:'xbtusd'})
-client.public('Ticker', {pair:'xxbtzusd'})
+// client.public('Ticker', {pair:'xxbtzusd'})
+// client.private('Balance')
+client.private('TradeBalance')
+// console.log('messageSignature: ',client.messageSignature('Balance'))
+// console.log('\ngetMessageSignature',client.getMessageSignature('Balance'))
+// console.log(process.env.API_SECRET)
+
--- kraken.js
+++ kraken.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const crypto = require('crypto')
 require('dotenv').config()
+const qs = require('qs')
 
 const serialize = (obj)=> {
   var str = [];
@@ -8,32 +9,36 @@ const serialize = (obj)=> {
     if (obj.hasOwnProperty(p)) {
       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
     }
-  return str.join("&");
+  return str.join("&")
 }
 
 const publicMethods = ['Time', 'Assets', 'AssetPairs', 'Ticker', 'OHLC', 'Depth', 'Trades', 'Spread']
-const privateMethods = ['Balance', 'TradeBalance', '']
+
+const privateMethods = ['Balance', 'TradeBalance', 'OpenOrders', 'ClosedOrders', 'QueryOrders', 'TradesHistory', 
+  'QueryTrades', 'OpenPositions', 'Ledgers', 'QueryLedgers', 'TradeVolume', 
+  'AddExport', 'ExportStatus', 'RetrieveReport', 'RemoveExport', 'AddOrder', 'CancelOrder']
+
 class Kraken {
   constructor() {
     this.API_KEY = process.env.API_KEY
     this.API_SECRET = process.env.API_SECRET
     this.URL = process.env.URL
-    this.nonce = new Date().getTime() * 1000
+    this.nonce = new Date().getTime()* 10000
   }
 
-  messageSignature (endpoint, postData=undefined) {
-    let path = `${this.URL}private/${endpoint}`
-    let data = postData === undefined ?  `${this.nonce}` : `${this.nonce}${serialize(postData)}`
-    let first = crypto.createHash('sha256').update(data).digest()
-    let base64decode = Buffer.from(this.API_SECRET, 'base64')
-    let second = crypto.createHmac('sha512', base64decode).update(first).digest()
-    return second 
+  messageSignature (endpoint, postData = undefined) {
+    const customPath = `/0/private/${endpoint}`
+    const post =  postData === undefined ?  `${this.nonce}` : `${this.nonce}nonce=${this.nonce}&${serialize(postData)}`   
+    const first = crypto.createHash('sha256').update(post).digest()
+    const base64decode = Buffer.from(this.API_SECRET, 'base64')
+    const second = crypto.createHmac('sha512', base64decode).update(`${customPath}${first}`).digest()
+    const final = Buffer.from(second).toString('base64')
+    return final  
   }
- 
 
   async public (endpoint, params = undefined) {
     if (publicMethods.includes(endpoint)) {
-      let url =  `${this.URL}public/${endpoint}?${serialize(params)}`
+      let url =  `${this.URL}/public/${endpoint}?${serialize(params)}`
         try {
           let response = await axios.get(url)
           console.log(response.data)
@@ -41,12 +46,30 @@ class Kraken {
           console.log(e)
         }
     } else {
-      console.log(`\n${endpoint} is not a valid public method`)
+      console.log(`\n${endpoint} is not a valid public endpoint`)
     }
   }
 
   async private (endpoint, params = undefined) {
-
+    if (privateMethods.includes(endpoint)) {
+      let url = params === undefined ? `${this.URL}/private/${endpoint}` : `${this.URL}/private/${endpoint}?${serialize(params)}`
+      try {
+        let response = await axios.post(url, {
+          headers: {
+            'User-Agent': 'kraken api axios- based Node JS client',
+            'API-Key': this.API_KEY,
+            'API-Sign': this.messageSignature(endpoint, params)
+          }
+        }, {
+          timeout: 5000
+        })
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      console.log(`\n${endpoint} is not a valid private endpoint`)
+    }
   }
  
 }
